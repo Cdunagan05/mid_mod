a123bb2acc3d697cd4f6109978ac4a43e343dca5
Chase Dunagan <cdunagan@utexas.edu>
2016-12-14 09:30:54 -0700

    add test to program

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'faraday'
 
 group :development, :test do
   gem 'byebug'
+  gem 'pry'
   gem 'rspec-rails'
   gem 'capybara'
   gem 'launchy'
---
+++ spec/services/nrel_services_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "NREL SERVICES" do
+  it "shows all the information needed from NREL API" do
+    # When I visit "/"
+    visit "/"
+
+    fill_in "q", with: "80203"
+    click_on "Locate"
+
+
+    # And I fill in the search form with 80203
+    # And I click "Locate"
+    # Then I should be on page "/search" with parameters visible in the url
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    # And the stations should be limited to Electric and Propane
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
+end
