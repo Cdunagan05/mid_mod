70c2257c7923e20172b3e538b3e8d9a572e18495
Chase Dunagan <cdunagan@utexas.edu>
2016-12-14 10:03:23 -0700

    add faraday to search controller, figured out test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: => "https://api.data.gov")
+    conn = Faraday.new(:url => "https://api.data.gov") do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    response = conn.get "/nrel/alt-fuel-stations/v1/nearest.json?api_key=ENV['API_KEY']&location=80203&fuel_type=ELEC,LPG&limit=10"
+    binding.pry
+    response.body
   end
 end
